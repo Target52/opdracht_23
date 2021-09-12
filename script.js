a67e27829c73b059f32327c1e6e0d05cf62e3294
Target52 <gulien@me.com>
2021-09-12 14:12:31 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+let makePizza = function () {
+    console.log("maak deeg");
+    console.log("rol deeg uit");
+    console.log("plaats saus");
+    console.log("plaats toppings");
+    console.log("bak in oven");
+}
+
+let makeSushi = function () {
+    console.log("kook rijst");
+    console.log("nij vis");
+    console.log("maak sushi");
+}
+
+makePizza();
+makePizza();
+makeSushi();
+makeSushi();
+makeSushi();
